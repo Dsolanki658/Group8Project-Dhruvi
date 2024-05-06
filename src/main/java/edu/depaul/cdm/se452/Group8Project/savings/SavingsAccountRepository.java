8f9b33cf4c801b31203bc3c9e20bff0fcf96c150
Dsolanki658 <166934166+Dsolanki658@users.noreply.github.com>
2024-05-06 15:53:43 -0500

    Update SavingsAccountRepository.java
    Made few changes

--- src/main/java/edu/depaul/cdm/se452/Group8Project/savings/SavingsAccountRepository.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/savings/SavingsAccountRepository.java
@@ -2,6 +2,6 @@ package edu.depaul.cdm.se452.Group8Project.checking;
 
 import edu.depaul.cdm.se452.Group8Project.account.AccountRepository;
 
-public interface SavingsAccountRepository extends AccountRepository<SavingsAccountAccount> {
+public interface SavingsAccountRepository extends AccountRepository<SavingsAccount> {
   
 }
