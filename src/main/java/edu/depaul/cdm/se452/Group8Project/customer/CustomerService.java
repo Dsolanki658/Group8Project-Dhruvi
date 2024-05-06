7c80cda005f2fe831d030a518739d294ff3e996c
Fernando <97924413+fernagomez00@users.noreply.github.com>
2024-05-06 12:50:20 -0500

    Updated CustomerService.java

--- src/main/java/edu/depaul/cdm/se452/Group8Project/customer/CustomerService.java
+++ src/main/java/edu/depaul/cdm/se452/Group8Project/customer/CustomerService.java
@@ -1,7 +1,5 @@
 package edu.depaul.cdm.se452.Group8Project.customer;
 
-import java.util.stream.StreamSupport;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +11,19 @@ public class CustomerService{
     @Autowired
     private CustomerRepository repository;
 
-    
+    public Account getChecking(long id){
+        return repository.findCA(id);
+    }
+
+    public Account getSaving(long id){
+        return repository.findSA(id);
+    }
+
+    public void save(Customer customer){
+        repository.save(customer);
+    }
+
+    public void delete(long id){
+        repository.delete(id);
+    }
 }
